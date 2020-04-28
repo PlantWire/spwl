54bae9debf4dffa2b369b9d249f83f85db348c90
SHafner <shafner@hsr.ch>
2020-04-28 14:40:05 +0200

    satisfy lint requirements

--- test/src/SPWL-test.cpp
+++ test/src/SPWL-test.cpp
@@ -9,55 +9,58 @@
 #include "../../lib/include/crc16.h"
 #include "../include/SPWL-test.h"
 
-const std::vector<unsigned char> hello {'H', 'e', 'l', 'l', 'o'};
-const std::vector<unsigned char> data1 {'M', 'y', 'L', 'i', 't', 't', 'l', 'e',
-                                  'C', 'p', 'p'};
-const std::vector<unsigned char> data2 { 'I', '\'', 'm', ' ', 'a', ' ', 'l', 'i', 't', 't', 'l', 'e', ' ', 'b', 'i', 't', ' ', 'l', 'o', 'n', 'g',
-		'e', 'r', ',', ' ', 'b', 'e', 'a', 'r', ' ', 'w', 'i', 't', 'h', ' ', 'm', 'e', '.', ' ', 'T', 'h', 'e', ' ', 'T', 'e', 's', 't', ' ', 's',
-		'h', 'o', 'u', 'l', 'd', ' ', 's', 't', 'i', 'l', 'l', ' ', 's', 'u', 'c', 'c', 'e', 'e', 'd', '.', '\n', 'T', 'h', 'i', 's', ' ', 'i', 's', ' ',
-		'i', 'm', 'p', 'o', 'r', 't', 'a', 'n', 't', ' ', 't', 'o', ' ', 'g', 'u', 'a', 'r', 'a', 'n', 't', 'e', 'e', ' ', 'i', 't', ' ', 'd', 'o', 'e',
-		's', 'n', '\'', 't', ' ', 'j', 'u', 's', 't', ' ', 't', 'e', 's', 't', 's', ' ', 'a', ' ', 'f', 'i', 'x', 'a', 't', 'e', 'd', ' ', 'r', 'e',
-		's', 'u', 'l', 't', '.' };
+const std::vector<unsigned char> hello { 'H', 'e', 'l', 'l', 'o' };
+const std::vector<unsigned char> data1 { 'M', 'y', 'L', 'i', 't', 't', 'l', 'e',
+    'C', 'p', 'p' };
+const std::vector<unsigned char> data2 { 'I', '\'', 'm', ' ', 'a', ' ', 'l',
+    'i', 't', 't', 'l', 'e', ' ', 'b', 'i', 't', ' ', 'l', 'o', 'n', 'g', 'e',
+    'r', ',', ' ', 'b', 'e', 'a', 'r', ' ', 'w', 'i', 't', 'h', ' ', 'm', 'e',
+    '.', ' ', 'T', 'h', 'e', ' ', 'T', 'e', 's', 't', ' ', 's', 'h', 'o', 'u',
+    'l', 'd', ' ', 's', 't', 'i', 'l', 'l', ' ', 's', 'u', 'c', 'c', 'e', 'e',
+    'd', '.', '\n', 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'i', 'm', 'p', 'o',
+    'r', 't', 'a', 'n', 't', ' ', 't', 'o', ' ', 'g', 'u', 'a', 'r', 'a', 'n',
+    't', 'e', 'e', ' ', 'i', 't', ' ', 'd', 'o', 'e', 's', 'n', '\'', 't', ' ',
+    'j', 'u', 's', 't', ' ', 't', 'e', 's', 't', 's', ' ', 'a', ' ', 'f', 'i',
+    'x', 'a', 't', 'e', 'd', ' ', 'r', 'e', 's', 'u', 'l', 't', '.' };
 
 // Begin CRC-Tests
 void crc1Test() {
-  CRC16 crc{};
-  crc.update(std::array<unsigned char, 9>{'1', '2', '3', '4', '5',
-                                          '6', '7', '8', '9'}, 9);
+  CRC16 crc { };
+  crc.update(std::array<unsigned char, 9> { '1', '2', '3', '4', '5', '6', '7',
+      '8', '9' }, 9);
   ASSERT_EQUALM("Encryption with CRC-16 doesn't match.", 0xbb3d, crc.get());
 }
 
 void crc2Test() {
-  CRC16 crc{};
-  crc.update(std::array<unsigned char, 10>{'H', 'e', 'l', 'l', 'o',
-                                          'W', 'o', 'r', 'l', 'd'}, 10);
+  CRC16 crc { };
+  crc.update(std::array<unsigned char, 10> { 'H', 'e', 'l', 'l', 'o', 'W', 'o',
+      'r', 'l', 'd' }, 10);
   ASSERT_EQUALM("Encryption with CRC-16 doesn't match.", 0x6053, crc.get());
 }
 
 void crc3Test() {
-  CRC16 crc{};
-  crc.update(std::array<unsigned char, 3>{'C', 'P', 'P'}, 3);
+  CRC16 crc { };
+  crc.update(std::array<unsigned char, 3> { 'C', 'P', 'P' }, 3);
   ASSERT_EQUALM("Encryption with CRC-16 doesn't match.", 0xe8cd, crc.get());
 }
 // End CRC-Tests
 
 // Begin SPWL-Tests
 void preambleCheckerTest() {
-  std::array<unsigned char, SPWLPacket::PREAMBLESIZE> input{};
+  std::array<unsigned char, SPWLPacket::PREAMBLESIZE> input { };
   std::string testPreamble = "UUUUUUU";
   ASSERTM("Empty preample was correct.", !SPWLPacket::checkPreamble(input));
 
   for (size_t i = 0; i < testPreamble.size(); i++) {
-	input[i] = testPreamble[i];
+    input[i] = testPreamble[i];
   }
   ASSERTM("Preample is wrong.", SPWLPacket::checkPreamble(input));
 }
 
 void packetTest() {
-  
   std::pair<SPWLPacket, bool> res = SPWLPacket::encapsulateData(hello);
   ASSERTM("Result is missing.", res.second);
-  SPWLPacket packet{res.first};
+  SPWLPacket packet { res.first };
   res = SPWLPacket::encapsulatePacket(packet.rawData());
   ASSERTM("Result is missing.", res.second);
   ASSERT_EQUALM("Data was not in encapsulation.", hello, res.first.getData());
@@ -65,7 +68,7 @@ void packetTest() {
 
 void corruptPacketTest() {
   std::pair<SPWLPacket, bool> res = SPWLPacket::encapsulateData(hello);
-  
+
   ASSERTM("Result is missing.", res.second);
   auto raw = res.first.rawData();
   raw[0] = 5;
@@ -75,7 +78,7 @@ void corruptPacketTest() {
 
 void corruptDataTest() {
   std::pair<SPWLPacket, bool> res = SPWLPacket::encapsulateData(hello);
-  
+
   ASSERTM("Result is missing.", res.second);
   auto raw = res.first.rawData();
   raw[SPWLPacket::PREAMBLESIZE + SPWLPacket::HEADERSIZE + 1] = 5;
@@ -84,25 +87,27 @@ void corruptDataTest() {
 }
 
 void packetMinTest() {
-  std::vector<unsigned char> data{'o'};
+  std::vector<unsigned char> data { 'o' };
   std::pair<SPWLPacket, bool> res = SPWLPacket::encapsulateData(data);
-  
+
   ASSERTM("Result is missing.", res.second);
-  SPWLPacket packet{res.first};
+  SPWLPacket packet { res.first };
   res = SPWLPacket::encapsulatePacket(packet.rawData());
   ASSERTM("Result is missing.", res.second);
-  ASSERT_EQUALM("Minimum sized data couldn't be encapsulated.", data, res.first.getData());
+  ASSERT_EQUALM("Minimum sized data couldn't be encapsulated.", data,
+      res.first.getData());
 }
 
 void packetMaxTest() {
   std::vector<unsigned char> data(SPWLPacket::MAXDATASIZE, 'h');
   std::pair<SPWLPacket, bool> res = SPWLPacket::encapsulateData(data);
-  
+
   ASSERTM("Result is missing.", res.second);
-  SPWLPacket packet{res.first};
+  SPWLPacket packet { res.first };
   res = SPWLPacket::encapsulatePacket(packet.rawData());
   ASSERTM("Result is missing.", res.second);
-  ASSERT_EQUALM("Maximum sized data couldn't be encapsulated.", data, res.first.getData());
+  ASSERT_EQUALM("Maximum sized data couldn't be encapsulated.", data,
+      res.first.getData());
 }
 
 void packetOverflowTest() {
@@ -113,11 +118,11 @@ void packetOverflowTest() {
 
 void lengthExtractorTest() {
   std::pair<SPWLPacket, bool> result = SPWLPacket::encapsulateData(data1);
-  
+
   ASSERTM("Result is missing.", result.second);
-  std::array<unsigned char, SPWLPacket::PACKETSIZE>
-      rawData{result.first.rawData()};
-  std::array<unsigned char, SPWLPacket::HEADERSIZE> header{};
+  std::array<unsigned char, SPWLPacket::PACKETSIZE> rawData {
+      result.first.rawData() };
+  std::array<unsigned char, SPWLPacket::HEADERSIZE> header { };
   std::copy(rawData.cbegin() + SPWLPacket::PREAMBLESIZE,
       rawData.cbegin() + SPWLPacket::PREAMBLESIZE + SPWLPacket::HEADERSIZE,
       header.begin());
@@ -128,11 +133,13 @@ void lengthExtractorTest() {
 void rawDataSizeTest() {
   std::pair<SPWLPacket, bool> result = SPWLPacket::encapsulateData(data1);
   ASSERTM("Result is missing.", result.second);
-  ASSERT_EQUALM("Data size wasn't correctly derived.", 7 + 6 + 11 + 2 + 1, result.first.rawDataSize());
+  ASSERT_EQUALM("Data size wasn't correctly derived.", 7 + 6 + 11 + 2 + 1,
+      result.first.rawDataSize());
 
   result = SPWLPacket::encapsulateData(data2);
   ASSERTM("Result is missing.", result.second);
-  ASSERT_EQUALM("Data size wasn't correctly derived.", 7 + 6 + data2.size() + 2 + 1, result.first.rawDataSize());
+  ASSERT_EQUALM("Data size wasn't correctly derived.",
+      7 + 6 + data2.size() + 2 + 1, result.first.rawDataSize());
 }
 // End SPWL-Tests
 bool runAllTests(int argc, char const *argv[]) {
